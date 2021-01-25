3d40ce43ccb16598452d288243a47fdbdb510102
Raul-Pinheiro <raulmatheus.rm@gmail.com>
2021-01-25 16:13:07 -0300

    Aula 1 - Desafio Metatag - Teste 2

--- src/components/MetaTag/index.js
+++ src/components/MetaTag/index.js
@@ -1,23 +1,18 @@
 import Head from 'next/head';
 import db from "../../../db.json";
 
-const  IndexPage = () => {
+function  IndexPage(){
     return (
       <div>
         <Head>
           <title>Next Question</title>
+          
+          <meta property="og:image" content={db.img} key="ogimage" />
           <meta property="og:title" content="Next Question" key="title" />
-        </Head>
-     
-        <Head>
-            <meta property="og:image" content={db.img} key="ogimage" />
-        </Head>     
-                  
-        <Head>     
-           <meta property="og:description" content={db.description} key="ogdesc" />
-        </Head>
-     
-        
+          <meta property="og:description" content={db.description} key="ogdesc" />
+
+        </Head>   
+                   
       </div>
     )
   }
